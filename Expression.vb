7841678713f4026d587773bd25f0b89977e6e8ef
Barret <format.e.drive@gmail.com>
2018-04-23 18:53:53 +0200

    Add files via upload

--- Expression.vb
+++ Expression.vb
@@ -4,7 +4,44 @@
         Me.Expression = expression
     End Sub
     Public Function Evaluate(Optional round As Boolean = False) As Double
-        Return Me.Evaluate(New Tree(New Tokenizer().Parse(Me.Expression)).Build).ToNumber(round)
+        Return Me.Evaluate(New Tree(Me.Tokenize(Me.Expression)).Build).ToNumber(round)
+    End Function
+    Public Function Tokenize(input As String) As List(Of Token)
+        Dim stream As New List(Of Token)
+        For i As Integer = 0 To input.Length - 1
+            Select Case input(i)
+                Case "+"c
+                    stream.Add(New Token(Tokens.PLUS, input(i), i))
+                Case "-"c
+                    stream.Add(New Token(Tokens.MINUS, input(i), i))
+                Case "*"c
+                    stream.Add(New Token(Tokens.MULT, input(i), i))
+                Case "/"c
+                    stream.Add(New Token(Tokens.DIV, input(i), i))
+                Case "%"c
+                    stream.Add(New Token(Tokens.MODULO, input(i), i))
+                Case "("c
+                    stream.Add(New Token(Tokens.PARENTHESIS_OPEN, input(i), i))
+                Case ")"c
+                    stream.Add(New Token(Tokens.PARENTHESIS_CLOSE, input(i), i))
+                Case "."c, "0"c To "9"c
+                    Dim index As Integer = i, value As String = String.Empty
+                    For j As Integer = index To input.Length - 1
+                        If (input(j).IsNumber Or input(j) = ".") Then
+                            value += input(j)
+                            i = j
+                            Continue For
+                        End If
+                        Exit For
+                    Next
+                    stream.Add(New Token(Tokens.NUMBER, value, index))
+                Case " "c
+                    Continue For
+                Case Else
+                    Throw New Exception(String.Format("unexpected character '{0}' at index {1}", input(i), i))
+            End Select
+        Next
+        Return stream
     End Function
     Private Function Evaluate(expression As Elements.Expression) As TValue
         Select Case expression.GetType
@@ -47,6 +84,8 @@
                     If (right.ToNumber >= 1) Then Return New TValue(left.ToNumber Mod right.ToNumber)
                     Throw New Exception(String.Format("division by zero '{0}'", expression))
             End Select
+        Else
+            Throw New Exception(String.Format("unexpected value '{0}'", right.Value))
         End If
         Throw New Exception(String.Format("undefined operator type '{0}'", expression.Operator))
     End Function
--- Settings.vb
+++ Settings.vb
@@ -1,14 +1,10 @@
 ﻿Imports System.Globalization
 
 Public Module Settings
-    Public Property Numbers As Char()
-    Public Property Operators As Char()
     Public Property Float As NumberStyles
     Public Property Culture As CultureInfo
     Sub New()
         Settings.Float = NumberStyles.Float
         Settings.Culture = New CultureInfo("US-en")
-        Settings.Operators = New Char() {"+"c, "-"c, "*"c, "/"c, "%"c}
-        Settings.Numbers = New Char() {"0"c, "1"c, "2"c, "3"c, "4"c, "5"c, "6"c, "7"c, "8"c, "9"c}
     End Sub
 End Module
