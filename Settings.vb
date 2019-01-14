88a3383567aad78c5f2d5c1b3fdf72e34d03c4c8
Barret <format.e.drive@gmail.com>
2019-01-14 22:16:28 +0100

    Fix: Culture & Max length

--- Settings.vb
+++ Settings.vb
@@ -1,13 +1,16 @@
-﻿Imports System.Globalization
+Imports System.Globalization
 Imports System.Runtime.InteropServices
 
 Public Module Settings
+    Public Property MaxLength As Integer
     Public Property Offset As Integer
     Public Property Float As NumberStyles
     Public Property Culture As CultureInfo
     Sub New()
+        Settings.MaxLength = 15
         Settings.Float = NumberStyles.Float
-        Settings.Culture = New CultureInfo("US-en")
+        Settings.Culture = New CultureInfo("en-US")
         Settings.Offset = Marshal.SizeOf(GetType(Double))
     End Sub
 End Module
+
